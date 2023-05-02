1abcdcffeca2911f580ed1a3f3992416fcf05741
Hoon9901 <shonn.dev@gmail.com>
2023-05-02 17:57:47 +0900

    chore: fix cors & db connection

---
+++ src/test/java/com/example/codebase/NyangPieceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.codebase;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyangPieceApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
