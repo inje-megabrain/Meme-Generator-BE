ab404a10fcbdd35a992fb9a1e3cd83ebe89ffc62
Hoon9901 <shonn.dev@gmail.com>
2023-06-07 11:50:53 +0900

    feat: Add oauth authentication exception

--- src/main/java/com/example/codebase/domain/auth/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/example/codebase/domain/auth/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,5 +1,7 @@
 package com.example.codebase.domain.auth.handler;
 
+import com.example.codebase.controller.dto.RestResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,16 @@ import java.io.IOException;
 
 @Component
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.getWriter().write("OAuth2 Authentication failed");
+        ObjectMapper mapper = new ObjectMapper();
+
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        RestResponse restResponse = new RestResponse(false, exception.getMessage(), HttpServletResponse.SC_BAD_REQUEST);
+        response.getWriter().write(mapper.writeValueAsString(restResponse));
     }
 }
